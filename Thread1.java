86e07a78eefbd12a79d361f8d85c10edd0830683
pranshu469 <pranshu469@gmail.com>
2019-08-24 13:17:27 +0530

    Update Thread1.java

--- Thread1.java
+++ Thread1.java
@@ -8,14 +8,11 @@ import java.io.InputStream;
 import java.text.DecimalFormat;
 import java.util.Properties;
 import java.util.Random;
-import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
 
 
 public class Thread1 extends Thread
 {
     
-    private static final Logger logger=Logger.getLogger(Thread1.class);
     static String path="C:\\Users\\Pranshu\\Documents\\NetBeansProjects\\09-07-19\\ServerFinal\\src\\java\\websocket\\server\\log4j.properties";
     static Properties p;
    
@@ -29,21 +26,17 @@ public class Thread1 extends Thread
     @Override
     public void run()
     {
-         
-         PropertyConfigurator.configure(path);
-        logger.info("in last5rows");
-        
-        p = new Properties();
+         p = new Properties();
          InputStream is = null;
         try {
             is = new FileInputStream(path);
         } catch (FileNotFoundException ex) {
-            logger.error(ex);
+            System.out.println(ex);
         }
         try {
             p.load(is);
         } catch (IOException ex) {
-            logger.error(ex);
+            System.out.println(ex);
         }
         
         String xyz=p.getProperty("threadtime");
@@ -81,12 +74,8 @@ public class Thread1 extends Thread
              
             String string=input.toString();
             CustomEndPoint.sendAll(string);
-            
-            
-          
-            logger.info("record");
+
             System.out.println();
-            
            
             try {
                 //Thread.sleep(time);
